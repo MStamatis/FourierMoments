d34e1fed2d68fc6b65f2790d6f34413ede0af6c3
Stamatis Mastromichalakis <stamatis@tmnetworks.gr>
2019-12-01 08:46:03 +0200

    clean

--- FourierMoments.py
+++ FourierMoments.py
@@ -17,28 +17,28 @@ def FourierMoments(S, a):
         https://www.researchgate.net/publication/320567826_A_Web_Based_Scheme_for_Image_Tomography_Applications
         in python
   """
-  n = a[0];
-  m = a[1];
+  n = a[0]
+  m = a[1]
   b=S.shape[0]
   bb=S.shape[1]
 
-  B1=np.zeros((b,n));
-  r=(n-1)/2;
+  B1=np.zeros((b,n))
+  r=(n-1)/2
   for l in range(1,n):
-      r2=(l-r-1);
+      r2=(l-r-1)
       for k in range(1,b):
-          z=-1j*pi*((k-1)*r2)/b;
-          B1[k,l]=np.exp(z);
-  B1=(1/sqrt(b))*B1;
+          z=-1j*pi*((k-1)*r2)/b
+          B1[k,l]=np.exp(z)
+  B1=(1/sqrt(b))*B1
 
-  B2=np.zeros((b,m));
-  r=(n-1)/2;
+  B2=np.zeros((b,m))
+  r=(n-1)/2
   for l in range(1,m):
-      r2=(l-r-1);
+      r2=(l-r-1)
       for k in range(1,bb):
-          z=-1j*pi*((k-1)*r2)/bb;
-          B2[k,l]=np.exp(z);
-  B2=(1/np.sqrt(bb))*B2;
+          z=-1j*pi*((k-1)*r2)/bb
+          B2[k,l]=np.exp(z)
+  B2=(1/np.sqrt(bb))*B2
 
   P = np.dot(np.dot(B1.T, np.double(S)), B2)
 
